3c1a8ab1c8cc2f0f61cd8c21fbdcbfbdd0fccb3a
Bert Jan Schrijver <bertjan@revolution.nl>
2019-06-14 14:41:08 +0200

    fix src directory, update scripts

--- install.sh
+++ install.sh
@@ -5,10 +5,13 @@ sudo yum install git
 sudo yum install gcc
 sudo yum install zlib-devel
 
-git clone https://github.com/pmlopes/aws-lambda-native-vertx
+git clone https://github.com/bertjan/aws-lambda-graal-native-image
 wget https://github.com/oracle/graal/releases/download/vm-1.0.0-rc16/graalvm-ce-1.0.0-rc16-linux-amd64.tar.gz
 tar xfzv graalvm-ce-1.0.0-rc16-linux-amd64.tar.gz
 
+aws iam delete-role --role-name lambda-role
 aws iam create-role --role-name lambda-role --path "/service-role/" --assume-role-policy-document file://policy.json
 
 # note the ARN that is returned, you need it in the next step
+
+cd aws-lambda-graal-native-image
